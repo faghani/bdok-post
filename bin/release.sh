e745155eac61f87405f42ee83e3c45fb42bde1a0
alireza faghani <faghani.a@gmail.com>
2017-07-01 15:30:21 +0430

    fix version name

--- bin/release.sh
+++ bin/release.sh
@@ -15,11 +15,13 @@ releaseBranch=release-$versionLabel
 git checkout -b $releaseBranch $devBranch
 
 # file in which to update version number
-versionFile="version.txt"
+versionFile="../version.txt"
 
 # find version number assignment ("= v1.5.5" for example)
 # and replace it with newly specified version number
 sed -i.backup -E "s/\= v[0-9.]+/\= $versionLabel/" $versionFile $versionFile
+
+slam
 
 # remove backup file created by sed command
 rm $versionFile.backup
